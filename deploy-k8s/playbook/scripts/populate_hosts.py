c158771505384e78d2d596605b8486ea2b044c48
Yadnesh Kulkarni <ykulkarn@redhat.com>
2021-03-18 14:46:54 +0530

    Ignore hostkey check Hardcode master node count to 1 for now Fix the approach to fetch IP address
    Signed-off-by: Yadnesh Kulkarni <ykulkarn@redhat.com>

--- deploy-k8s/playbook/scripts/populate_hosts.py
+++ deploy-k8s/playbook/scripts/populate_hosts.py
@@ -1,10 +1,24 @@
 import sys
 import subprocess
+import ipaddress
+import time
 
 vm = sys.argv[1]
 
-proc=subprocess.Popen(["virsh", "domifaddr", vm],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-stdout,stderr=proc.communicate()
-if not stderr:
-    ip_addr = stdout.decode("utf-8").split()[-1].split("/")[0]
-    print(ip_addr)
+def try_fetching_ip(vm):
+    proc=subprocess.Popen(["virsh", "domifaddr", vm],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    stdout,stderr=proc.communicate()
+    if not stderr:
+        ip_addr = stdout.decode("utf-8").split()[-1].split("/")[0]
+        return ip_addr
+
+while True:
+    out = try_fetching_ip(vm)
+    try:
+        ipaddress.ip_address(out)
+        print(out)
+        break
+    except ValueError:
+        continue
+    time.sleep(2)
+
